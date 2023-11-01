a03ed4e70c494a4d6eb6f8a28cfbcadfa4b26d2b
Jozef Majzel <jozefmajzel1@gmail.com>
2023-11-01 13:53:58 +0100

    deps: Add svg loader.

--- packages/nova/vite.config.ts
+++ packages/nova/vite.config.ts
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite'
 import {resolve} from 'path'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   build: {
     lib: {
       // src/index.ts is where we have exported the component(s)
