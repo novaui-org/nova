9c43d7d36260f7530e638b1d475733fea02465bc
Jozef Majzel <jozefmajzel1@gmail.com>
2023-12-02 18:22:33 +0100

    featL Create ripple composable and ripple Motion element.

---
+++ packages/components/src/components/NRipple/NRipple.vue
@@ -0,0 +1,50 @@
+<template>
+  <Motion
+    class="n-ripple"
+    :initial="{transform: 'scale(0)', opacity: 0.35}"
+    :animate="{transform: 'scale(2)', opacity: 0}"
+    :exit="{opacity: 0}"
+    :transition="{duration}"
+    :style="styles"
+    @motionstart="$emit('ripple:start', {event: $event, key: ripple.key})"
+    @motioncomplete="$emit('ripple:end', {event: $event, key: ripple.key})"
+  />
+</template>
+
+<script setup lang="ts">
+import {Motion} from 'motion/vue'
+import {NRippleEmits, NRippleProps} from 'src/components/NRipple/types.ts'
+import {computed, CSSProperties} from 'vue'
+
+const props = withDefaults(defineProps<NRippleProps>(), {
+  color: 'currentColor',
+})
+
+defineEmits<NRippleEmits>()
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max)
+}
+
+const duration = computed(() => clamp(0.01 * props.ripple.size, 0.2, props.ripple.size > 100 ? 0.75 : 0.5))
+
+const styles = computed<CSSProperties>(() => {
+  return {
+    backgroundColor: props.color,
+    top: `${props.ripple.y}px`,
+    left: `${props.ripple.x}px`,
+    width: `${props.ripple.size}px`,
+    height: `${props.ripple.size}px`,
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.n-ripple {
+  position: absolute;
+  border-radius: 100%;
+  transform-origin: center;
+  pointer-events: none;
+  z-index: 10;
+}
+</style>
---
+++ packages/components/src/composable/ripple.ts
@@ -0,0 +1,54 @@
+import {ref} from 'vue'
+
+export type RippleType = {
+  key: string
+  x: number
+  y: number
+  size: number
+}
+
+
+export function getUniqueID(prefix: string) {
+  return `${prefix}-${Math.floor(Math.random() * 1000000)}`
+}
+
+export function getNumericStyleProperty(htmlElement: HTMLElement, property: string) {
+  const style = getComputedStyle(htmlElement, null)
+  return parseInt(style.getPropertyValue(property), 10)
+}
+
+export function useRipple() {
+  const ripples = ref<RippleType[]>([])
+
+  function onClick(event: MouseEvent) {
+    if (!(event.currentTarget instanceof HTMLElement)) return
+    const trigger = event.currentTarget
+
+    const size = Math.max(trigger.clientWidth, trigger.clientHeight)
+    const rect = trigger.getBoundingClientRect()
+
+    /* Calculate border offset */
+    const borderTop = getNumericStyleProperty(trigger, 'border-top-width')
+    const borderLeft = getNumericStyleProperty(trigger, 'border-left-width')
+
+    ripples.value.push({
+      key: getUniqueID(`ripples-${ripples.value.length}`),
+      size,
+      x: event.clientX - rect.x - (size / 2) - borderLeft,
+      y: event.clientY - rect.y - (size / 2) - borderTop,
+    })
+  }
+
+  function onClear(key: string) {
+    ripples.value = ripples.value.filter(ripple => ripple.key !== key)
+  }
+
+  return {
+    ripples,
+    onClick,
+    onClear,
+  }
+}
+
+
+
