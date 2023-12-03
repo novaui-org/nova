a43debc9d314f03108ae033401e570c1f25c5185
Jozef Majzel <jozefmajzel1@gmail.com>
2023-12-03 12:53:00 +0100

    chore: Simplify exports.,

---
+++ packages/components/src/components/index.ts
@@ -0,0 +1,3 @@
+export {default as NButton} from 'src/components/NButton/NButton.vue'
+export {default as NIcon} from 'src/components/NIcon/NIcon.vue'
+export {default as NText} from 'src/components/NText/NText.vue'
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,12 +1,3 @@
 import 'src/styles/index.global.scss'
-import NButton from 'src/components/NButton/NButton.vue'
-import NIcon from 'src/components/NIcon/NIcon.vue'
-import NText from 'src/components/NText/NText.vue'
-import * as Utils from 'src/utils/index.ts'
-
-export {
-    NButton,
-    NIcon,
-    NText,
-    Utils,
-}
+export * from 'src/components/index.ts'
+export * as Utils from 'src/utils/index.ts'
