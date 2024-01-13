42c4bcbb005fdbfc61280b614ea9f18f36f8546e
Jozef Majzel <jozefmajzel1@gmail.com>
2024-01-13 16:07:31 +0100

    feat(NDivider): Create divider component.

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -1,3 +1,4 @@
 export {default as NButton} from 'src/components/NButton/NButton.vue'
 export {default as NIcon} from 'src/components/NIcon/NIcon.vue'
 export {default as NText} from 'src/components/NText/NText.vue'
+export {default as NDivider} from 'src/components/NDivider/NDivider.vue'
