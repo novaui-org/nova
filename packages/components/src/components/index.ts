ec71dd0e1efa36b2e0efc658b15b9e41488ab9d3
Jozef Majzel <jozefmajzel1@gmail.com>
2024-02-25 19:46:01 +0100

    feat(components/exports): Properly export both vue and types.

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -1,6 +1,26 @@
 export {default as NButton} from 'src/components/NButton/NButton.vue'
+export * from 'src/components/NButton/types.ts'
+
 export {default as NIcon} from 'src/components/NIcon/NIcon.vue'
+export * from 'src/components/NIcon/types.ts'
+
 export {default as NText} from 'src/components/NText/NText.vue'
+export * from 'src/components/NText/types.ts'
+
 export {default as NDivider} from 'src/components/NDivider/NDivider.vue'
+export * from 'src/components/NDivider/types.ts'
+
 export {default as NCard} from 'src/components/NCard/NCard.vue'
+export * from 'src/components/NCard/types.ts'
+
 export {default as NRadio} from 'src/components/NRadio/NRadio.vue'
+export * from 'src/components/NRadio/types.ts'
+
+export {default as NRadioGroup} from 'src/components/NRadioGroup/NRadioGroup.vue'
+export * from 'src/components/NRadioGroup/types.ts'
+
+export {default as NSlideTransition} from 'src/components/NSlideTransition/NSlideTransition.vue'
+export * from 'src/components/NSlideTransition/types.ts'
+
+export {default as NInputMessage} from 'src/components/NInputMessage/NInputMessage.vue'
+export * from 'src/components/NInputMessage/types.ts'
