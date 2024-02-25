927ff64014dea481d03065a1db093ed33f5c31ed
Jozef Majzel <jozefmajzel1@gmail.com>
2024-02-25 19:45:30 +0100

    feat(components/NRadioGroup): Add error/success icon, errorMessage and error styling.

--- packages/components/src/components/NRadioGroup/NRadioGroup.vue
+++ packages/components/src/components/NRadioGroup/NRadioGroup.vue
@@ -1,37 +1,103 @@
 <template>
-  <div class="n-radio-group" :class="{'n-radio-group--disabled': disabled}" role="radiogroup">
-    <span class="n-radio-group__label" v-text="label"/>
-    <slot/>
+  <div
+    class="n-radio-group"
+    :class="{
+      'n-radio-group--disabled': disabled,
+      'n-radio-group--error': !!error,
+    }"
+    role="radiogroup"
+  >
+    <div class="n-radio-group__label-container">
+      <span class="n-radio-group__label" v-text="label"/>
+
+      <n-slide-transition :visible="error || success" variant="both">
+        <n-icon
+          :name="error ? errorIcon : successIcon"
+          :color="error ? 'n-danger' : 'n-success'"
+        />
+      </n-slide-transition>
+    </div>
+
+    <div class="n-radio-group__content">
+      <slot/>
+    </div>
+
+    <n-input-message class="n-radio-group__message" variant="negative" :visible="!!error" :message="errorMessage"/>
   </div>
 </template>
 
 <script setup lang="ts">
 import {NRadioGroupProps} from 'src/components/NRadioGroup/types.ts'
+import NInputMessage from 'src/components/NInputMessage/NInputMessage.vue'
+import NIcon from 'src/components/NIcon/NIcon.vue'
+import {NSlideTransition} from 'src/components'
 
-defineProps<NRadioGroupProps>()
+withDefaults(defineProps<NRadioGroupProps>(), {
+  errorIcon: 'vuesax/linear/warning-2',
+  successIcon: 'vuesax/linear/tick-circle',
+})
 </script>
 
 <style scoped lang="scss">
 .n-radio-group {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  gap: $n-space-8;
+  .n-radio-group__label-container {
+    display: flex;
+    gap: $n-space-8;
+    align-items: center;
 
-  .n-radio-group__label {
-    line-height: 1.5;
-    font-size: 1rem;
-    color: $n-default-50;
-    font-weight: 400;
+    .n-radio-group__label {
+      display: inline-block;
+      line-height: 1.5;
+      font-size: 1rem;
+      color: $n-default-50;
+      font-weight: 400;
+    }
+  }
+
+  .n-radio-group__content {
+    display: flex;
+    flex-direction: column;
+    gap: $n-space-8;
+    margin-top: $n-space-8;
+  }
+
+  .n-radio-group__message {
+    margin-top: $n-space-8;
   }
 
   &.n-radio-group--disabled {
     opacity: 0.5;
     pointer-events: none;
 
-    :deep(.n-radio.n-radio--disabled) {
-      /* Prevent double changing of opacity for nested NRadio. */
-      opacity: initial;
+    .n-radio-group__content {
+      :deep(.n-radio.n-radio--disabled) {
+        /* Prevent double changing of opacity for nested NRadio. */
+        opacity: initial;
+      }
+    }
+  }
+
+  &.n-radio-group--error {
+    .n-radio-group__label-container {
+      .n-radio-group__label {
+        color: $n-danger-50;
+      }
+    }
+
+    :deep(.n-radio) {
+      .n-radio__checkmark {
+        border-color: $n-danger;
+
+        .n-radio__checkmark-inner {
+          background-color: $n-danger;
+        }
+      }
+
+      .n-radio__label-container {
+        .n-radio__label {
+          color: $n-danger;
+        }
+      }
     }
   }
 }
--- packages/components/src/components/NRadioGroup/types.ts
+++ packages/components/src/components/NRadioGroup/types.ts
@@ -1,4 +1,11 @@
+import NovaIcons from '@nova/icons'
+
 export interface NRadioGroupProps {
   label: string
   disabled?: boolean
+  error?: boolean
+  success?: boolean
+  errorMessage?: string
+  errorIcon?: keyof typeof NovaIcons
+  successIcon?: keyof typeof NovaIcons
 }
