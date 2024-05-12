4ddc33644e1ab90c7d630f472e664756d9b4708d
Jozef Majzel <jozefmajzel1@gmail.com>
2024-05-12 17:58:05 +0200

    docs(@nova-org/docs/DisplayContainer): Create simple reusable display container.

---
+++ packages/docs/examples/partials/DisplayContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <n-card class="display-container n-mt-16" variant="outline" radius="md" size="md">
+    <div class="display-container__inner">
+      <slot/>
+    </div>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import {NCard} from '@nova-org/components'
+</script>
+
+<style scoped lang="scss">
+.display-container {
+  padding: $n-space-16 $n-space-8;
+
+  .display-container__inner {
+    display: flex;
+    flex-wrap: wrap;
+    gap: $n-space-12;
+  }
+}
+</style>
