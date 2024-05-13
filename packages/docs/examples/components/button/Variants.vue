d1c0acd996bd8f5e2585f40b06afb201d7bc9687
Jozef Majzel <jozefmajzel1@gmail.com>
2024-05-13 21:36:53 +0200

    docs(@nova-org/docs/Button): Rewrite whole Button.md page.

---
+++ packages/docs/examples/components/button/Variants.vue
@@ -0,0 +1,21 @@
+<!-- #region template -->
+<template>
+  <n-button variant="solid" label="Solid"/>
+  <n-button variant="shadow" label="Shadow"/>
+  <n-button variant="ghost" label="Ghost"/>
+  <n-button variant="outline" label="Outline"/>
+  <n-button variant="flat" label="Flat"/>
+</template>
+<!-- #endregion template -->
+
+<!-- #region script -->
+<script setup lang="ts">
+import {NButton} from '@nova-org/components'
+</script>
+<!-- #endregion script -->
+
+<!-- #region style -->
+<style scoped lang="scss">
+
+</style>
+<!-- #endregion style -->
---
+++ packages/docs/examples/components/button/WithIcon.vue
@@ -0,0 +1,19 @@
+<!-- #region template -->
+<template>
+  <n-button icon-left="vuesax/linear/add-circle" label="Icon left"/>
+  <n-button icon-right="vuesax/linear/add-circle" label="Icon right"/>
+  <n-button icon-left="vuesax/linear/add-circle" icon-right="vuesax/linear/add-circle" label="Icons both"/>
+</template>
+<!-- #endregion template -->
+
+<!-- #region script -->
+<script setup lang="ts">
+import {NButton} from '@nova-org/components'
+</script>
+<!-- #endregion script -->
+
+<!-- #region style -->
+<style scoped lang="scss">
+
+</style>
+<!-- #endregion style -->
