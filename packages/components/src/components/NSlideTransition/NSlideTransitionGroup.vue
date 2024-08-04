f3f835f121bcceb2c4a344bb86d4457edbd794fe
Jozef Majzel <jozefmajzel1@gmail.com>
2024-08-04 18:08:26 +0200

    feat(components/NSlideTransitionsGroup): create also transition-group component

---
+++ packages/components/src/components/NSlideTransition/NSlideTransitionGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition-group
+    :css="false"
+    :persisted="false"
+    @enter="enterTransition"
+    @leave="leaveTransition"
+  >
+    <slot/>
+  </transition-group>
+</template>
+
+<script setup lang="ts">
+import {type NSlideTransitionProps} from './types'
+import {useSlideTransition} from 'src/components/NSlideTransition/use-slide-transition'
+
+const props = withDefaults(defineProps<NSlideTransitionProps>(), {
+  duration: 300,
+})
+const {enterTransition, leaveTransition} = useSlideTransition(props)
+</script>
+
+<style scoped lang="scss">
+</style>
