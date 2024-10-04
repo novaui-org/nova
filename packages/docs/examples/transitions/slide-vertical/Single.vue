b93490a31311c8a3bbb2e94fa020928a88415240
Jozef Majzel <jozefmajzel1@gmail.com>
2024-10-04 22:12:27 +0200

    feat(docs/SlideHorizontal): add docs for vertical animation transition

---
+++ packages/docs/examples/transitions/slide-vertical/Single.vue
@@ -0,0 +1,27 @@
+<!-- #region template -->
+<template>
+  <n-checkbox v-model="modelValue" label="Show card bellow"/>
+
+  <n-slide-transition>
+    <n-card v-if="modelValue" size="sm">
+      <n-text weight="medium" font-size="paragraph" html-tag="div" v-text="'Small'"/>
+      <n-text weight="light" font-size="small" html-tag="div" v-text="'Modern and responsive.'"/>
+    </n-card>
+  </n-slide-transition>
+</template>
+<!-- #endregion template -->
+
+<!-- #region script -->
+<script setup lang="ts">
+import {NCard, NCheckbox, NSlideTransition, NText} from '@nova-org/components'
+import {ref} from 'vue'
+
+const modelValue = ref<boolean>(true)
+</script>
+<!-- #endregion script -->
+
+<!-- #region style -->
+<style scoped lang="scss">
+
+</style>
+<!-- #endregion style -->
