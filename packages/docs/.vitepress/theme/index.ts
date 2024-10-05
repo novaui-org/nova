b19dbce3315400a9a843f5ee4d1ce4f33cace81d
Jozef Majzel <jozefmajzel1@gmail.com>
2024-10-05 21:57:10 +0200

    feat(docs): style theme a bit

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -2,8 +2,8 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
 import '@nova-org/components/style.css'
+import './style.css'
 
 export default {
   extends: DefaultTheme,
