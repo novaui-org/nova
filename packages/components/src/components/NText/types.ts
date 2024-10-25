7b00930aaff9b06262542bffa7c096e2d758061a
Jozef Majzel <jozefmajzel1@gmail.com>
2024-10-25 21:47:32 +0200

    feat(components): add paragraph-2 font size

--- packages/components/src/components/NText/types.ts
+++ packages/components/src/components/NText/types.ts
@@ -1,7 +1,7 @@
 
 export interface NTextProps {
   weight?: 'thin' | 'light' | 'regular' | 'medium' | 'bold' | 'bolder'
-  fontSize?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' |  'paragraph' | 'small'
+  fontSize?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' |  'paragraph' | 'small' | 'paragraph-2'
   htmlTag?: string
   visibleLines?: number
 }
